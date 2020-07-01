3a9b3d756d0e746cf9f91292637995f4f6f3a76e
Saadoune <sanaasdn@hotmail.com>
2020-07-01 01:33:12 +0100

    Event Page Done

--- Model/Cloture.php
+++ Model/Cloture.php
@@ -14,36 +14,50 @@
         header("Location:../View/LoginTest.php");
     }
     else{
-        $stmt = $db->prepare("SELECT  nom_event FROM evenement WHERE id_event = ? LIMIT 1");
+        $stmt = $db->prepare("SELECT  nom_event , date_event FROM evenement WHERE id_event = ? LIMIT 1");
         $stmt->execute(array($id_event));
         $row = $stmt->fetch();
     }
     
     if(isset($_POST['Cloturer']))
         {
+            $stmt2 = $db->prepare("SELECT  nom_event, date_event FROM evenement WHERE id_event = ? LIMIT 1");
+            $stmt2->execute(array($id_event));
+            $row2 = $stmt2->fetch();
+            $photo_name = $row2->nom_event.$row2->date_event;
+            
+
             $article = checkInput($_POST['article']);
             $photo1 = checkInput($_FILES['photo1']['name']);
             $photo2= checkInput($_FILES['photo2']['name']);
             $photo3 = checkInput($_FILES['photo3']['name']);
             $photo4 = checkInput($_FILES['photo4']['name']);
-            $target = "../Public/Images_event/". $_SESSION['id_club']."/".basename($photo1);
-            $target = "../Public/Images_event/". $_SESSION['id_club']."/".basename($photo2);
-            $target = "../Public/Images_event/". $_SESSION['id_club']."/".basename($photo3);
-            $target = "../Public/Images_event/". $_SESSION['id_club']."/".basename($photo4);
-            $imageExtension  = pathinfo($target,PATHINFO_EXTENSION);
+
+            $photorandom1 = rand(0,1000000);
+            $target1 = "../Public/Images_event/". $_SESSION['id_club']."/".$photorandom1.basename($photo1);
+            $photorandom2 = rand(0,1000000);
+            $target2 = "../Public/Images_event/". $_SESSION['id_club']."/".$photorandom2.basename($photo2);
+            $photorandom3 = rand(0,1000000);
+            $target3 = "../Public/Images_event/". $_SESSION['id_club']."/".$photorandom3.basename($photo3);
+            $photorandom4 = rand(0,1000000);
+            $target4 = "../Public/Images_event/". $_SESSION['id_club']."/".$photorandom4.basename($photo4);
+
+            $imageExtension  = pathinfo($target1,PATHINFO_EXTENSION);
+            
             if(empty($article)){
                     $articleERROR = "Ce champ ne peut pas être vide";
                     $isSuccess = false;
             }
-            if(empty($photo1)||empty($photo2)||empty($photo3)||empty($photo4)||empty($photo5)){
-                $photoERROR = "Veuillez ajouter 5 images!";
+            if(empty($photo1)||empty($photo2)||empty($photo3)||empty($photo4)){
+                $photoERROR = "Veuillez ajouter 4 images!";
                 $isSuccess = false;
             }
-            else if($imageExtension != "jpg" && $imageExtension != "png" && $imageExtension != "jpeg" && $imageExtension != "gif"){
+            else if(!checkExtension($photo1,$target1) || !checkExtension($photo2,$target2) || !checkExtension($photo3,$target3) || !checkExtension($photo4,$target4) )
+            {
                 $photoERROR = "Les fichiers autorisés sont : .jpg, .jpng , .png , .gif";
-                $isSuccess = false;   
+                $isSuccess = false;
             }
-            else if($_FILES["photo1"]["size"] > 10000000 || $_FILES["photo2"]["size"] > 10000000 || $_FILES["photo3"]["size"] > 10000000 || $_FILES["photo4"]["size"] > 10000000 || $_FILES["photo5"]["size"] > 10000000){ 
+            else if($_FILES["photo1"]["size"] > 10000000 || $_FILES["photo2"]["size"] > 10000000 || $_FILES["photo3"]["size"] > 10000000 || $_FILES["photo4"]["size"] > 10000000){ 
                 $photoERROR = "Veuillez revoir les tailles des images";
                 $isSuccess = false;
             }
@@ -51,16 +65,16 @@
              
         if($isSuccess){
             $status = false;
-            move_uploaded_file($_FILES['photo1']['tmp_name'],$target); 
-            move_uploaded_file($_FILES['photo2']['tmp_name'],$target); 
-            move_uploaded_file($_FILES['photo3']['tmp_name'],$target);  
-            move_uploaded_file($_FILES['photo4']['tmp_name'],$target); 
+            move_uploaded_file($_FILES['photo1']['tmp_name'],$target1); 
+            move_uploaded_file($_FILES['photo2']['tmp_name'],$target2); 
+            move_uploaded_file($_FILES['photo3']['tmp_name'],$target3);  
+            move_uploaded_file($_FILES['photo4']['tmp_name'],$target4); 
 
             $stmt = $db->prepare("INSERT INTO photo(src_photo,id_event) VALUES (?, ?)");
-            $stmt->execute(array($photo1, $id_event));
-            $stmt->execute(array($photo2, $id_event));
-            $stmt->execute(array($photo3, $id_event));
-            $stmt->execute(array($photo4, $id_event));
+            $stmt->execute(array($photorandom1.$photo1, $id_event));
+            $stmt->execute(array($photorandom2.$photo2, $id_event));
+            $stmt->execute(array($photorandom3.$photo3, $id_event));
+            $stmt->execute(array($photorandom4.$photo4, $id_event));
 
             $stmt2 = $db->prepare("UPDATE evenement SET article = ? , cloture = 1   WHERE id_event= ?");
             $stmt2->execute(array($article, $id_event));
--- View/Evenements.php
+++ View/Evenements.php
@@ -12,7 +12,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-    <title> Les evenements </title>
+    <title> Les événements </title>
 </head>
 <body>
     <?php 
@@ -35,17 +35,21 @@
         <div class="space"></div>
         <div class="tri">
             <div class="triAll">
-                <select id="tri_date">
-                    <option hidden disabled selected value="valeur1">Tri par date</option> 
-                    <option value="valeur2" >Valeur 2</option>
-                    <option value="valeur3">Valeur 3</option>
+            <form method="post">
+                <select name="select1" id="tri_date">
+                    <option hidden  selected value="valeur1">Tri par date</option> 
+                    <?php foreach($rows3 as $row3) { ?>
+                        <option  value="<?php echo $row3->date_event ?>" ><?php echo $row3->date_event ?></option>
+                    <?php } ?>
                 </select>
-                <select id="tri_date">
-                    <option  hidden disabled selected value="valeur1">Tri par club</option> 
-                    <option value="valeur2" >Valeur 2</option>
-                    <option value="valeur3">Valeur 3</option>
+                <select  name="select2" id="tri_club">
+                    <option  hidden selected value="valeur1">Tri par club</option> 
+                    <?php foreach($rows2 as $row2) { ?>
+                        <option value="<?php echo $row2->nom_club ?>" ><?php echo $row2->nom_club ?></option>
+                    <?php } ?>
                 </select>
-                <button class="btnSearch"> <img src="../Public/Images/search.png" height="30px" width="30px" alt="search"></button>
+                <button class="btnSearch" name="Search"> <img src="../Public/Images/search.png" height="30px" width="30px" alt="search"></button>
+            </form>
             </div>
         </div>
 
